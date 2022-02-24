1f6e3e3136683d0332bf190203a03a6f3f763f01
Thomas-2003 <thomasroenneper12@web.de>
2022-02-24 09:28:22 +0100

    style userlist, name too short alert

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const names = ["Thomas", "Frank", "Tommy"] // nono-react pure javascript array
   const [stateNames, setStateNames] = useState(names) //define a getter variable and a setter method
   const [input, setInput] = useState("")
+  const [error, setError] = useState(undefined)
 
   function convert(input) {
     let output = "";
@@ -19,19 +20,31 @@ function App() {
     <div className="App">
       <header className="App-header">
         Hey new user {input}!
-        <input id="name" name="username" placeholder='Enter name' onChange={(e) => {
-          const binaryString = convert(e.target.value)
-          setInput(binaryString)
-        }} />
-        <button onClick={(e) => { // react has lots of build in event handlers
-          const name = document.getElementById("name")
-          const newStateNames = [...stateNames] // deepclone
-          newStateNames.push(name.value)
-          setStateNames(newStateNames)
-        }}>Submit</button>
-        <ul>
-          <Userlist stateNames={stateNames} setStateNames={setStateNames} />
-        </ul>
+        <div class="input-group my-3">
+          <input className='form-control' value={input} id="name" name="username" placeholder='Enter name' onChange={(e) => {
+            const binaryString = convert(e.target.value)
+            setInput(e.target.value)
+            if (e.target.value.length > 1) {
+              setError(undefined)
+            } else {
+              setError("Name too short")
+            }
+          }} />
+
+          <button className="btn btn-primary ms-2" onClick={(e) => { // react has lots of build in event handlers
+            const name = document.getElementById("name")
+            const newStateNames = [...stateNames] // deepclone
+            newStateNames.push(name.value)
+            setStateNames(newStateNames)
+          }}>Submit</button>
+        </div>
+        {error !== undefined ? (
+          <div class="mb-3 alert alert-danger" role="alert">
+            {error}
+          </div>
+
+        ) : null}
+        <Userlist stateNames={stateNames} setStateNames={setStateNames} />
 
       </header>
     </div>
--- src/Userlist.jsx
+++ src/Userlist.jsx
@@ -1,12 +1,14 @@
 function Userlist(props) {
-    return props.stateNames.map(name => (
-        <li>{name} <button onClick={(e) => {
-            const newStateNames = [...props.stateNames] // deepclone
-            const positionInArray = newStateNames.findIndex(nameToSearch => nameToSearch === name)
-            const personDeleted = newStateNames.splice(positionInArray, 1)
-            props.setStateNames(newStateNames)
-        }}>X</button></li>
-
-    ))
+    return <ul className="list-group">{props.stateNames.map(name => (
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+            <span className="me-3">{name}</span>
+            <button className="btn btn-outline-danger" onClick={(e) => {
+                const newStateNames = [...props.stateNames] // deepclone
+                const positionInArray = newStateNames.findIndex(nameToSearch => nameToSearch === name)
+                const personDeleted = newStateNames.splice(positionInArray, 1)
+                props.setStateNames(newStateNames)
+            }}>X</button></li>
+    ))}
+    </ul>
 }
 export default Userlist
