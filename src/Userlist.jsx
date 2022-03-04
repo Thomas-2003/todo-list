fdc4357d96868f3c5ed33a20baebdaf7b36520fa
Thomas-2003 <thomasroenneper12@web.de>
2022-03-04 08:37:07 +0100

    make todo item editable with conditional input

--- src/Userlist.jsx
+++ src/Userlist.jsx
@@ -7,22 +7,43 @@ function Userlist(props) {
         inputRefs.push(ref);
     };
     const [edit, setEdit] = useState("")
-    console.log(edit)
     const [title, setTitle] = useState("")
-    return <ul className="list-group w-100">{props.stateNames.map(todo => (
-        <li className="list-group-item d-flex justify-content-between align-items-center">
-            <span ref={setRef} contentEditable={edit === todo.id ? "true" : "false"} className="me-3">{todo.title}</span>
-            <button className="btn btn-outline-info" onClick={(e) => {
-                setEdit(todo.id)
-                inputRefs[todo.id].focus();
-
-            }}>E</button>
-            <button className="btn btn-outline-danger" onClick={(e) => {
-                const newStateNames = [...props.stateNames] // deepclone
-                const positionInArray = newStateNames.findIndex(nameToSearch => nameToSearch.title === todo.title)
-                const personDeleted = newStateNames.splice(positionInArray, 1)
-                props.setStateNames(newStateNames)
-            }}>X</button></li>
+    console.log(edit)
+    return <ul className="list-group w-100">{props.stateNames.map((todo, index) => (
+        <li className={`list-group-item d-flex justify-content-between align-items-center ${edit === index ? `ps-1` : ``}`}>
+            {edit === index ? (
+                <>
+                    <input className="form-control" value={todo.title} />
+                </>
+            ) : (
+                <>
+                    <span ref={setRef} className="me-3">{todo.title}</span>
+                </>
+            )}
+            <div className='d-flex'>
+                {edit === index ? (
+                    <>
+                        <button className="btn btn-outline-success" onClick={(e) => {
+                            setEdit(index)
+                            //  inputRefs[todo.id].focus();
+                        }}>S</button>
+                    </>
+                ) : (
+                    <>
+                        <button className="btn btn-outline-info" onClick={(e) => {
+                            setEdit(index)
+                            //  inputRefs[todo.id].focus();
+                        }}>E</button>
+                    </>
+                )}
+                <button className="btn btn-outline-danger" onClick={(e) => {
+                    const newStateNames = [...props.stateNames] // deepclone
+                    const positionInArray = newStateNames.findIndex(nameToSearch => nameToSearch.title === todo.title)
+                    const personDeleted = newStateNames.splice(positionInArray, 1)
+                    props.setStateNames(newStateNames)
+                }}>X</button>
+            </div>
+        </li>
     ))}
     </ul>
 }
