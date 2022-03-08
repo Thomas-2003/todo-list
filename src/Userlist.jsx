fbe2f033ad314b78523c334eb811415340414f23
Thomas-2003 <thomasroenneper12@web.de>
2022-03-08 10:19:10 +0100

    hacky way to access not yet renderd ref through timeout

--- src/Userlist.jsx
+++ src/Userlist.jsx
@@ -1,44 +1,30 @@
 
-import React, { useState, useEffect, useMemo, useRef } from 'react'
+import React, { useState, createRef } from 'react'
 function Userlist(props) {
-    const inputRefs = useRef([]);
-
-
-
+    const lineRefs = React.useRef([]);
+    lineRefs.current = props.stateNames.map((_, i) => lineRefs.current[i] ?? createRef());
     const [edit, setEdit] = useState("")
-    const [title, setTitle] = useState("")
-    // inputRefs.current = props.stateNames.map((element, i) => inputRefs.current[i] ?? createRef());
-    const refsById = {}
-    props.stateNames.forEach((item, index) => {
-        refsById[index] = React.createRef(null)
-    })
     return <ul className={`list-group w-100`}>{props.stateNames.map((todo, index) => (
         <li key={index} className={`list-group-item d-flex justify-content-between align-items-center  ${props.darkmode ? `list-group-item-dark bg-dark text-white border-white` : ``} ${edit === index ? `ps-1` : ``}`}>
-            {edit === index ? (
-                <>
-                    <input ref={refsById[index]} className={`form-control ${props.darkmode ? `bg-dark text-light` : ``}`} onChange={e => {
-                        console.log(e)
-                    }} value={todo.title} />
-                </>
-            ) : (
-                <>
-                    <span className="me-3">{todo.title}</span>
-                </>
-            )}
+
+            <input ref={lineRefs.current[index]} className={`form-control ${props.darkmode ? `bg-dark text-light` : ``} ${edit === index ? `` : `d-none`}`} value={todo.title} />
+
+            <span className={`me-3  ${edit === index ? `d-none` : ``}`}>{todo.title}</span>
+
             <div className='d-flex'>
                 {edit === index ? (
                     <>
                         <button className="btn btn-outline-success" onClick={(e) => {
                             setEdit(index)
-                            //  inputRefs[todo.id].focus();
                         }}>S</button>
                     </>
                 ) : (
                     <>
                         <button className="btn btn-outline-info" onClick={(e) => {
                             setEdit(index)
-                            console.log(refsById[index])
-                            // inputRefs.current[index].current.focus();
+                            setTimeout(() => {
+                                lineRefs.current[index].current.focus();
+                            }, 1);
                         }}>E</button>
                     </>
                 )}
